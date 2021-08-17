aa7c83e0e9c42c2cb6a6251057b0c6efbd89dfe4
Oleg Zharov <levineye@gmail.com>
2021-08-17 19:35:36 +0300

    feat: создать файл для типов и интерфейсов

---
+++ src/utils/types.ts
@@ -0,0 +1,30 @@
+export interface IRequestHeader {
+  Accept?: string;
+  'Content-Type'?: string;
+  Authorization: string;
+}
+
+export interface IResponce<T> {
+  readonly ok: boolean;
+  readonly status: number;
+  readonly statusText: string;
+  readonly headers: object;
+  readonly json: () => Promise<T>;
+  readonly text: () => Promise<T>;
+}
+
+export interface IApiError {
+  message: string;
+  status: number;
+  statusText: string;
+}
+
+export interface IHttpMethod {
+  OPTIONS: string;
+  HEAD: string;
+  GET: string;
+  POST: string;
+  PUT: string;
+  PATCH: string;
+  DELETE: string;
+}
