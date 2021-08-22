a374cf7dc696d415c46ed42940f8faaece4e61a0
Oleg Zharov <levineye@gmail.com>
2021-08-22 19:39:46 +0300

    feat: переписать попап с картинкой на ts

---
+++ src/components/PopupWithImage.ts
@@ -0,0 +1,26 @@
+import Popup from './Popup.js';
+
+class PopupWithImage extends Popup {
+  private _image: HTMLImageElement;
+  private _title: HTMLHeadingElement;
+
+  constructor(popupSelector: string) {
+    super(popupSelector);
+    this._image = document.querySelector(
+      '.popup__card-img'
+    ) as HTMLImageElement;
+    this._title = document.querySelector(
+      '.popup__title-img'
+    ) as HTMLHeadingElement;
+  }
+
+  public open(title?: string, link?: string): void {
+    this._title.textContent = title || '';
+    this._image.src = link || '';
+    this._image.alt = title || '';
+
+    super.open();
+  }
+}
+
+export default PopupWithImage;
