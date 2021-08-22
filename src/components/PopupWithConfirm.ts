c23b1a8b51f6dfe7eeadbcda15d4742ad2bcd4b0
Oleg Zharov <levineye@gmail.com>
2021-08-22 20:58:00 +0300

    feat: переписать попап подтверждения на ts

---
+++ src/components/PopupWithConfirm.ts
@@ -0,0 +1,53 @@
+import Popup from './Popup';
+
+class PopupWithConfirm extends Popup {
+  private _form: HTMLFormElement;
+
+  public constructor(
+    popupSelector: string,
+    private _handleSubmitCallback: (id: string, markup: HTMLElement) => void,
+    private _itemId: string = '',
+    private _itemMarkup: HTMLElement | null = null
+  ) {
+    super(popupSelector);
+    this._form = this._popup.querySelector('.popup__form') as HTMLFormElement;
+  }
+
+  private _handleSubmit(evt: Event): void {
+    evt.preventDefault();
+
+    if (this._itemId && this._itemMarkup) {
+      this._handleSubmitCallback(this._itemId, this._itemMarkup);
+    }
+  }
+
+  protected _setEventListeners(): void {
+    super._setEventListeners();
+    this._form.addEventListener('submit', this._handleSubmit);
+  }
+
+  protected _removeEventListeners(): void {
+    super._removeEventListeners();
+    this._form.removeEventListener('submit', this._handleSubmit);
+  }
+
+  public set setItemId(id: string) {
+    this._itemId = id;
+  }
+
+  public set setItemMarkup(markup: HTMLElement) {
+    this._itemMarkup = markup;
+  }
+
+  public open(): void {
+    super.open();
+    this._setEventListeners();
+  }
+
+  public close(): void {
+    super.close();
+    this._removeEventListeners();
+  }
+}
+
+export default PopupWithConfirm;
