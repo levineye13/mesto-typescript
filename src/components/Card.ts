76fbf3a8b6656c2997d74de64cfe963b567845f1
Oleg Zharov <levineye@gmail.com>
2021-08-25 18:50:19 +0300

    feat: добавить переключение класса кнопки

--- src/components/Card.ts
+++ src/components/Card.ts
@@ -6,28 +6,28 @@ class Card {
   private _isLiked: boolean = false;
   private _likeNumbers: number = 0;
   private _handleClickCallback: (name: string, link: string) => void;
-  private _handleLikeCallback: (cardElement: HTMLLIElement) => void;
+  private _handleLikeCallback: () => void;
+  private _handleDislikeCallback: () => void;
   private _handleDeleteCallback: (cardElement: HTMLLIElement) => void;
 
-  public constructor(card: {
-    selector: string;
-    data: ICard;
-    handleClickCallback: (name: string, link: string) => void;
-    handleLikeCallback: (cardElement: HTMLLIElement) => void;
-    handleDeleteCallback: (cardElement: HTMLLIElement) => void;
-  }) {
-    this._cardSelector = card.selector;
+  public constructor(
+    selector: string,
+    card: {
+      data: ICard;
+      handleClickCallback: (name: string, link: string) => void;
+      handleLikeCallback: () => void;
+      handleDislikeCallback: () => void;
+      handleDeleteCallback: (cardElement: HTMLLIElement) => void;
+    }
+  ) {
+    this._cardSelector = selector;
     this._data = card.data;
     this._handleClickCallback = card.handleClickCallback;
     this._handleLikeCallback = card.handleLikeCallback;
+    this._handleDislikeCallback = card.handleDislikeCallback;
     this._handleDeleteCallback = card.handleDeleteCallback;
   }
 
-  /**
-   * Вернуть шаблон карточки
-   * @return {object} Возвращает шаблон разметки
-   * @private
-   */
   private _getTemplate(): HTMLLIElement {
     const template: HTMLTemplateElement = document.querySelector(
       this._cardSelector
@@ -87,16 +87,32 @@ class Card {
   //   cardElement = null;
   // }
 
-  private _like(likeElement: HTMLSpanElement): void {
+  public get getLikeState(): boolean {
+    return this._isLiked;
+  }
+
+  public get getId(): string {
+    return this._data._id;
+  }
+
+  private _like(
+    likeElement: HTMLSpanElement,
+    likeButton: HTMLButtonElement
+  ): void {
     this._isLiked = true;
     this._likeNumbers++;
     likeElement.textContent = this._likeNumbers.toString();
+    likeButton.classList.add('elements__like-button_active');
   }
 
-  private _dislike(likeElement: HTMLSpanElement): void {
+  private _dislike(
+    likeElement: HTMLSpanElement,
+    likeButton: HTMLButtonElement
+  ): void {
     this._isLiked = false;
     this._likeNumbers--;
     likeElement.textContent = this._likeNumbers.toString();
+    likeButton.classList.remove('elements__like-button_active');
   }
 
   private _handleClick(name: string, link: string): void {
@@ -108,25 +124,33 @@ class Card {
     cardElement.remove();
   }
 
-  private _handleChangeLike(
-    cardElement: HTMLLIElement,
-    likeElement: HTMLSpanElement
-  ) {
-    this._handleLikeCallback(cardElement);
+  private _handleLike(
+    likeElement: HTMLSpanElement,
+    likeButton: HTMLButtonElement
+  ): void {
+    this._handleLikeCallback();
+    this._like(likeElement, likeButton);
+  }
 
+  private _handleDislike(
+    likeElement: HTMLSpanElement,
+    likeButton: HTMLButtonElement
+  ): void {
+    this._handleDislikeCallback();
+    this._dislike(likeElement, likeButton);
+  }
+
+  private _handleChangeLike(
+    likeElement: HTMLSpanElement,
+    likeButton: HTMLButtonElement
+  ) {
     if (this._isLiked) {
-      this._dislike(likeElement);
+      this._handleDislike(likeElement, likeButton);
     } else {
-      this._like(likeElement);
+      this._handleLike(likeElement, likeButton);
     }
   }
 
-  // /**
-  //  * Обработчики событий
-  //  *
-  //  * @param  {object} cardElement - разметка карточки
-  //  * @private
-  //  */
   private _setEventListeners(
     cardElement: HTMLLIElement,
     likeElement: HTMLSpanElement
@@ -138,11 +162,13 @@ class Card {
       this._handleDelete(cardElement);
     });
 
-    (
-      cardElement.querySelector('.elements__like-button') as HTMLButtonElement
-    ).addEventListener('click', (evt) => {
+    const likeButton: HTMLButtonElement = cardElement.querySelector(
+      '.elements__like-button'
+    ) as HTMLButtonElement;
+
+    likeButton.addEventListener('click', (evt) => {
       evt.stopPropagation();
-      this._handleChangeLike(cardElement, likeElement);
+      this._handleChangeLike(likeElement, likeButton);
     });
 
     cardElement.addEventListener('click', () =>
