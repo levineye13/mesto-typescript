9294d20034b96736d41241bb9ca7b8e8fc5ea34b
Oleg Zharov <levineye@gmail.com>
2021-09-07 20:15:30 +0300

    feat: добавить файл с типами для типизации экшенов

---
+++ src/redux/types.ts
@@ -0,0 +1,11 @@
+import { Action } from 'redux';
+
+import { IUser } from '../utils/interfaces';
+import { SET_USER } from './constants';
+
+export interface SetUser extends Action {
+  type: typeof SET_USER;
+  payload: IUser;
+}
+
+export type ActionType = SetUser;
