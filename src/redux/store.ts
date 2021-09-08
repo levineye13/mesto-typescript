ff52d2b2efefff1b2d5d1c7e11e04751133e2cb6
Oleg Zharov <levineye@gmail.com>
2021-09-08 22:37:53 +0300

    feat: добавить редакс девтулс и написать мидлвару логгера стейта

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
-import { createStore, Store } from 'redux';
+import { ActionType } from './types';
+import { combinedStateType } from './state';
+import { applyMiddleware, createStore, Store } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from './reducers';
+import { logger } from './middlewares';
 
-const store: Store = createStore(rootReducer);
+const store: Store<combinedStateType, ActionType> = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export { store };
