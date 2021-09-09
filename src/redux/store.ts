ccfce3a68ef7964d1c70b457979a323247f2243c
Oleg Zharov <levineye@gmail.com>
2021-09-09 20:00:56 +0300

    feat: подключить redux-thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,14 @@
-import { ActionType } from './types';
-import { combinedStateType } from './state';
 import { applyMiddleware, createStore, Store } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import { ActionType } from './types';
+import { combinedStateType } from './state';
 import { rootReducer } from './reducers';
-import { logger } from './middlewares';
 
 const store: Store<combinedStateType, ActionType> = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export { store };
