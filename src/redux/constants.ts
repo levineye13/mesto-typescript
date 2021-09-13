28747a2e5717d83a1a8a9b62206f23f2a5b58d45
Oleg Zharov <levineye@gmail.com>
2021-09-13 19:58:13 +0300

    feat: добавить константы типа

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -1,2 +1,4 @@
 export const SET_USER: string = 'SET_USER';
+export const UPDATE_USER: string = 'UPDATE_USER';
+export const UPDATE_AVATAR: string = 'UPDATE_AVATAR';
 export const SET_CARDS: string = 'SET_CARDS';
