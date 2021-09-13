5223fff2c6863a49fdd7d9b5933db63e5bab1476
Oleg Zharov <levineye@gmail.com>
2021-09-13 19:57:36 +0300

    feat: добавить экшены для юзера

--- src/redux/actions/user.ts
+++ src/redux/actions/user.ts
@@ -1,8 +1,21 @@
+import { SET_USER, UPDATE_USER } from './../constants';
 import { IUser } from '../../utils/interfaces';
-import { SetUser } from '../types';
-import { SET_USER } from '../constants';
+import { SetUser, UpdateAvatar, UpdateUser } from '../types';
 
 export const setUser = (user: IUser): SetUser => ({
   type: SET_USER,
+  payload: user,
+});
+
+export const updateUser = (user: {
+  name: string;
+  about: string;
+}): UpdateUser => ({
+  type: UPDATE_USER,
+  payload: user,
+});
+
+export const updateAvatar = (user: { avatar: string }): UpdateAvatar => ({
+  type: UPDATE_USER,
   payload: user,
 });
