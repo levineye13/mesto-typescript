1a7164410b1425468075e14b35cd87058512468b
Oleg Zharov <levineye@gmail.com>
2021-09-13 19:56:49 +0300

    feat: добавить асинхронные экшены для юзера

--- src/redux/actions/async/user.ts
+++ src/redux/actions/async/user.ts
@@ -1,10 +1,7 @@
-import { ThunkAction, ThunkDispatch } from 'redux-thunk';
-
 import ApiUser from '../../../utils/api/ApiUser';
 import { IUser } from '../../../utils/interfaces';
-import { RootState } from '../../state';
-import { AppActionTypes, AppThunkAction, AppThunkDispatch } from '../../types';
-import { setUser } from '../';
+import { AppThunkAction, AppThunkDispatch } from '../../types';
+import { setUser, updateUser, updateAvatar } from '../';
 
 export const fetchUser =
   (api: ApiUser): AppThunkAction<Promise<void>> =>
@@ -12,4 +9,20 @@ export const fetchUser =
     const user: IUser = await api.getUser();
 
     dispatch(setUser(user));
+  };
+
+export const fetchUpdateUser =
+  (api: ApiUser, user: { name: string; about: string }): AppThunkAction =>
+  async (dispatch: AppThunkDispatch) => {
+    const updatedUser: IUser = await api.editProfile(user);
+
+    dispatch(updateUser(updatedUser));
+  };
+
+export const fetchUpdateAvatar =
+  (api: ApiUser, avatar: string): AppThunkAction =>
+  async (dispatch: AppThunkDispatch) => {
+    const updatedUser: IUser = await api.updateAvatar(avatar);
+
+    dispatch(updateAvatar(updatedUser));
   };
